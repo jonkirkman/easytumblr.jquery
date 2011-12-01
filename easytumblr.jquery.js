87173fa2aac0666e657008921dd415168e091531
Jon Kirkman <jon@vstm.us>
2011-12-01 12:20:22 -0500

    Changed code formatting for better readability.

--- easytumblr.jquery.js
+++ easytumblr.jquery.js
@@ -6,7 +6,8 @@
 		Created by Jon Kirkman - Versus The Moon
 	*/
 
-	$.fn.getTumblr = function(options) {
+	$.fn.getTumblr = function(options) 
+	{
 
 		$.fn.getTumblr.defaults = {
 			userName: null,
@@ -18,19 +19,25 @@
 		};
 		var o = $.extend({}, $.fn.getTwitter.defaults, options);
 
-		function truncate (text, limit, append) {
+		function _truncate (text, limit, append) 
+		{
 		    if (typeof text !== 'string')
 		        return '';
+
 		    if (typeof append == 'undefined')
 		        append = '...';
+
 		    var parts = text.split(' ');
-		    if (parts.length > limit) {
+
+		    if (parts.length > limit) 
+		    {
 		        // loop backward through the string
-		        for (var i = parts.length - 1; i > -1; --i) {
+		        for (var i = parts.length - 1; i > -1; --i) 
+		        {
 		            // if i is over limit, drop this word from the array
-		            if (i+1 > limit) {
+		            if (i+1 > limit)
 		                parts.length = i;
-		            }
+
 		        }
 		        // add the truncate append text
 		        parts.push(append);
@@ -39,11 +46,15 @@
 		    return parts.join(' ');
 		}
 
-		return this.each(function(){
+		return this.each(function()
+		{
 			var container = $(this);
 			$.getJSON( 'http://'+o.userName+'.tumblr.com/api/read/json?num='+o.numPosts+'&filter=text&callback=?', function(tumblr){
+
 				container.append('<ul style="display:none;"></ul>');
-				$(tumblr.posts).each(function(){
+
+				$(tumblr.posts).each(function()
+				{
 					var title = (typeof(this['regular-title']) != 'undefined' && typeof(this['url-with-slug']) != 'undefined')? '<'+o.titleElement+'><a href="'+this['url-with-slug']+'">'+this['regular-title']+'</a></'+o.titleElement+'>' : '';
 					var body = (typeof(this['regular-body']) != 'undefined')? '<'+o.bodyElement+'>'+truncate(this['regular-body'], o.bodyLimit)+'</'+o.bodyElement+'>' : '';
 					container.children('ul').append('<li>'+title+body+'</li>');
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 *easytumblr*
 
 This script is used to pull feeds from tumblr using jQuery.
+
 Enjoy.
